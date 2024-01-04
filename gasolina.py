f33a8002fa8495028774842e5eb580cef7aca91b
paulo-silveira <paulo.seraville@gmail.com>
2024-01-04 22:31:08 +0000

    arquivo gasolina.py, gasolina.csv e gasolina png foram adicionados

--- gasolina.py
+++ gasolina.py
@@ -1,1 +1,17 @@
-codigo_python
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+caminho_do_arquivo = '/content//ebac-2/gasolina.csv'
+dados = pd.read_csv(caminho_do_arquivo)
+
+sns.lineplot(x='dia', y='venda', data=dados, palette='viridis')
+
+plt.title('Gráfico do preço da gasolina ao longo dos dias')
+plt.xlabel('Dia')
+plt.ylabel('Preço')
+plt.savefig('/content/ebac-2/gasolina.png')
+
+
+with open('/content/ebac-2/gasolina.py', 'w') as f:
+    f.write('codigo_python')
